dcb5e589da274de4d69c18b0540c22d7eb0aaf6a
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-05-03 22:20:38 +0700

    Progress hospital

--- hospital.js
+++ hospital.js
@@ -1,17 +1,72 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 class Hospital {
-  constructor(name, location, employees, patients) {
+  constructor(name, location) {
     this.name = name
-    this.employees = employees
-    this.patients = patients
+    this.employees = []
+    // this.patients = patients
     this.location = location
+  }
+
+  addEmployees(object){
+    return this.employees.push(object)
+  }
+
+  login(){
+    rl.question('Please enter your username: ', (answer) => {
+      let loginFlag = false
+      for(let i=0; i<this.employees.length; i++){
+        if(this.employees[i]._username==answer){
+          loginFlag = true
+        }
+      }
+      if(loginFlag){
+        console.log('===========================================')
+        this.password()
+      } else {
+        console.log('===========================================')
+        this.login()
+      }
+    })
+  }
+
+  password(){
+    rl.question('Please enter your password: ', (answer) => {
+      let passFlag = false
+      for(let i=0; i<this.employees.length; i++){
+        if(this.employees[i]._password==answer){
+          passFlag = true
+        }
+      }
+      if(passFlag){
+        console.log('===========================================')
+        console.log('sampe sini dulu')
+      } else {
+        console.log('===========================================')
+        this.password()
+      }
+    })
   }
 }
 
 class Patient {
   constructor(id, name, diagnosis) {
-    this.id = id
-    this.name = name
-    this.diagnosis = diagnosis
+    this._id = id
+    this._name = name
+    this._diagnosis = diagnosis
+  }
+  get id(){
+    return this._id
+  }
+  get name(){
+    return this._name
+  }
+  get diagnosis(){
+    return this._diagnosis
   }
 }
 
@@ -19,7 +74,45 @@ class Employee {
   constructor(name, position, username, password) {
     this.name = name
     this.position = position
-    this.username = username
-    this.password = password
+    this._username = username
+    this._password = password
+  }
+  get username(){
+    return this._username
+  }
+  get password(){
+    return this._password
   }
 }
+
+// function readline(message) {
+//   const readline = require('readline');
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
+//   rl.setPrompt(message);
+//   rl.prompt();
+//
+//   rl.on('line', (line) => {
+//     return line
+//   }).on('close', ()=> {
+//     console.log('Have a great day!');
+//     process.exit(0);
+//   })
+// }
+
+function runner() {
+  console.log(`Welcome to ${medika.name} Hospital`)
+  console.log(medika.location)
+  console.log('===========================================')
+  medika.login()
+}
+
+let medika = new Hospital('Medika Center', 'Jl. Kebayoran Lama No.64, Jakarta')
+medika.addEmployees(new Employee('Abdi Dia', 'Admin', 'abdi', '123'))
+medika.addEmployees(new Employee('Beta Taro', 'Dokter', 'beta', '123'))
+medika.addEmployees(new Employee('Delta Tama', 'OfficeBoy', 'delta', '123'))
+// medika.addEmployees(new Employee('Carli Lima', 'Pasien', 'carli', '123'))
+runner()
+// console.log(medika.login('betas'))

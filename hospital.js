2fc374046abafec86515c165926d9e278629509b
Antoni Angga <antoniangga14@gmail.com>
2017-05-05 00:42:25 +0700

    Sudah Selesai untuk view Report, Login ke privilage nya lagi. Masalah nya adalah : 1. how to track ID when i write the ID ?, jika terjawab sudah bisa semua.

--- hospital.js
+++ hospital.js
@@ -10,7 +10,8 @@ class Hospital {
         this.name = name;
         this.location = location;
         this.dataPatient = [];
-        this.dataEmployee = new Employee("Angga","Admin","Angga","1234");
+        this.dataEmployee = [];
+        this.UserLogin = [];
     }
 
     welcome() {
@@ -20,76 +21,207 @@ class Hospital {
         console.log(nameRumahSakit);
         console.log(addrRumahSakit);
         console.log(garisGambar);
-        //Tanya Username nya
-        rl.question("Please input your Username :",(Username) =>{
-          if(Username === ES["username"]){
-            //Sudah Masuk
-            this.InputPassword();
-          }
-          else{
-            this.Reset();
-            console.log("Username Salah");
-            console.log("\n");
-            this.welcome();
-          }
+        rl.question("[1].Patient\n[2].Employee\nWhich one you want login : ",(Jawaban) =>{
+            if(Jawaban === "1" || Jawaban === "Patient"){
+              console.log("Patient Comming Soon");
+            }
+            else if(Jawaban === "2" || Jawaban === "Employee"){
+              this.LoginEmployee();
+            }
+            else{
+                this.ResetLine();
+                console.log("Sorry must be 1 or 2, you typing Patient or Employee ");
+                this.welcome();
+            }
+        });
+    }
+
+    LoginEmployee(){
+      this.ResetLine();
+        rl.question("\nPlease input your Username :",(Username) =>{
+                this.dataEmployee.map((data) =>{
+                    if(data.username === Username){
+                        this.UserLogin.push(data.name,data.password,data.position);
+                        this.LoginPassword();
+                    }
+                });
+                if(!this.UserLogin){
+                    this.ResetLine();
+                    console.log("Username Salah, Mohon Coba Lagi");
+                    this.LoginEmployee();
+                }
             }
         );
     }
 
-    Reset(){
-      console.log("\x1B[2J");
+    LoginPassword(){
+        this.ResetLine();
+        rl.question("Please Input your Password :",(Password) =>{
+            if(Password === this.UserLogin[1]){
+                this.WelcomeUser();
+            }
+            else{
+                console.log("Password Salah, Mohon Coba lagi");
+                this.LoginPassword();
+            }
+        })
+    }
+    //Pemilihan Menu dari 0 - 8
+    ChooseMenu(){
+        rl.question("\n Please input your choose here : ", (Choose)=>{
+            switch(Choose){
+                case "1":
+                    //List Patient
+                    this.ReportPatient();
+                    break;
+                case "2":
+                    //View Record
+                    this.DetailPatient();
+                    break;
+                case "3" :
+                    //Add Patient
+                    this.addPatient();
+                    break;
+
+                case "4" :
+                    //Remove Patient
+                    this.DeletePatient();
+                    break;
+
+                case "5" :
+                    //Add Employee
+                    this.addEmployee();
+                    break;
+
+                case "6" :
+                    //List Employee
+                    this.ReportEmployees();
+                    break;
+
+                case "7" :
+                    //Delete Employee
+                    this.DeleteEmployee();
+                    break;
+                case "0" :
+                    //Logout
+                    this.UserLogin = [];
+                    this.ResetLine();
+                    this.welcome();
+                    break;
+
+                default : console.log("Reset");
+                break;
+            }
+        })
+    }
+
+    ReportPatient(){
+        this.ResetLine();
+        console.log("=======Report Patient=====");
+        console.log("ID | Patient Name | Diagnosis");
+        this.dataPatient.map((dataPatient) =>{
+            console.log(`${dataPatient.id} | ${dataPatient.name} | ${dataPatient.diagnosis}`);
+        } )
+    }
+
+    ReportEmployees(){
+        this.ResetLine();
+        console.log("=======Report Employess=====");
+        console.log("No | Employee Name | Position");
+        let NO = 0;
+        this.dataEmployee.map((dataEmployees) =>{
+            NO++;
+            console.log(`${NO} | ${dataEmployees.name} | ${dataEmployees.position}`);
+        } )
+    }
+
+    DetailPatient(){
+        console.log(idPatient);
+        this.dataPatient.map((dataPatient) => {
+            if(dataPatient.id.toString() === idPatient) {
+                console.log(`Patient Detail Record, ${dataPatient.name}`);
+                console.log(`=============================================`);
+                console.log(`ID : ${dataPatient.id} | Name : ${dataPatient.name} | Diagnosis : ${dataPatient.diagnosis}`);
+            }
+        });
+    }
+
+    addEmployee(data){
+        if(data !== undefined){
+            this.dataEmployee.push(data);
+        }
+    }
+
+    addPatient(data){
+        if(data !== undefined){
+            this.dataPatient.push(data);
+        }
+    }
+
+    DeletePatient(){
+
+    }
+
+    DeleteEmployee(){
+
     }
 
     WelcomeUser(){
-      return `Welcome, ${ES["name"]}. Your access level is : ${ES["position"]}`;
+        this.ResetLine();
+        console.log("==============================================");
+        console.log(`=Welcome, ${this.UserLogin[0]}. Your access level is : ${this.UserLogin[2]}=`);
+        console.log("==============================================");
+        this.MainMenu(this.UserLogin[2]);
     }
 
-    InputPassword(){
-        //Tanya Password nya
-        rl.question("Please Input your Password :",(Password) => {
-                if(Password === ES["password"]){
-                  //Sudah Masuk
-                    switch (ES["position"]){
-                        case "Admin" :
-                          console.log("This Name");
-                          break;
-                        case "Patient" :
-                          break;
-                        case "Doktor" :
-                          break;
-                        case "Office Boy":
-                          break;``
-                    }
+    MainMenu(position){
+        console.log("=========Please Input the Menu Number=========");
+        switch(position){
+            //Admin Bisa Semua
+            case "Admin" : console.log("\n[1].List Patient \n[2].View Record \n[3].Add Patient\n[4].Remove_Patient\n[5].Add Employee\n[6].List Employee\n[7].Remove Employee\n[0].Logout");
+            break;
+            //Doctor hanya untuk patient
+            case "Doctor" : console.log("\n[1].List Patient\n[2].View Record\n[3].Add Patient\n[4].Remove_Patient\n[0].Logout");
+            break;
+            //Hanya bisa lihat Record nya sendiri
+            case "Patient" : console.log("\n[1].View Record\n[0].Logout");
+            break;
+            // Logout Doang
+            case "Office Boy" : console.log("[0].Logout");
+            break;
+        }
+        this.ChooseMenu();
+    }
 
-                }
-                else{
-                    this.Reset();
-                    console.log("Password Salah, Tolong coba lagi !");
-                    this.InputPassword();
-                }
-            }
-        );
+
+    ResetLine(){
+        console.log("\x1B[2J");
     }
 
 }
 
 class Patient {
   constructor(id, name, diagnosis) {
-    this.id = id
-    this.name = name
-    this.diagnosis = diagnosis
+    this.id = id;
+    this.name = name;
+    this.diagnosis = diagnosis;
   }
 }
 
 class Employee {
   constructor(name, position, username, password) {
-    this.name = name
-    this.position = position
-    this.username = username
-    this.password = password
+    this.name = name;
+    this.position = position;
+    this.username = username;
+    this.password = password;
   }
 }
 
 const Rs = new Hospital("Welcome Rumah Sakit Berkah","Jalan Ahamad yani no 21");
+Rs.addEmployee(new Employee("Angga","Admin","Angga","123456"));
+Rs.addEmployee(new Employee("Budi","Office Boy","Budi","123456"));
+Rs.addEmployee(new Employee("Lina","Doctor","Lina","123456"));
+Rs.addPatient(new Patient("001","Joe","Sakit Gigi"));
+
 
 Rs.welcome();

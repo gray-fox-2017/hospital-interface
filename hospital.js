6e259d03678e4d8306a06703683b29e861bb4782
Antoni Angga <antoniangga14@gmail.com>
2017-05-04 17:57:02 +0700

    untuk model sudah okay, tinggal logic Datanya untuk bisa akses ke Employee yang baru2 lagi

--- hospital.js
+++ hospital.js
@@ -1,10 +1,76 @@
+//use readline to fix this challenge
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 class Hospital {
-  constructor(name, location, employees, patients) {
-    this.name = name
-    this.employees = employees
-    this.patients = patients
-    this.location = location
-  }
+    constructor(name, location) {
+        this.name = name;
+        this.location = location;
+        this.dataPatient = [];
+        this.dataEmployee = new Employee("Angga","Admin","Angga","1234");
+    }
+
+    welcome() {
+        var nameRumahSakit = this.name;
+        var addrRumahSakit = this.location;
+        var garisGambar = "===========================";
+        console.log(nameRumahSakit);
+        console.log(addrRumahSakit);
+        console.log(garisGambar);
+        //Tanya Username nya
+        rl.question("Please input your Username :",(Username) =>{
+          if(Username === ES["username"]){
+            //Sudah Masuk
+            this.InputPassword();
+          }
+          else{
+            this.Reset();
+            console.log("Username Salah");
+            console.log("\n");
+            this.welcome();
+          }
+            }
+        );
+    }
+
+    Reset(){
+      console.log("\x1B[2J");
+    }
+
+    WelcomeUser(){
+      return `Welcome, ${ES["name"]}. Your access level is : ${ES["position"]}`;
+    }
+
+    InputPassword(){
+        //Tanya Password nya
+        rl.question("Please Input your Password :",(Password) => {
+                if(Password === ES["password"]){
+                  //Sudah Masuk
+                    switch (ES["position"]){
+                        case "Admin" :
+                          console.log("This Name");
+                          break;
+                        case "Patient" :
+                          break;
+                        case "Doktor" :
+                          break;
+                        case "Office Boy":
+                          break;``
+                    }
+
+                }
+                else{
+                    this.Reset();
+                    console.log("Password Salah, Tolong coba lagi !");
+                    this.InputPassword();
+                }
+            }
+        );
+    }
+
 }
 
 class Patient {
@@ -23,3 +89,7 @@ class Employee {
     this.password = password
   }
 }
+
+const Rs = new Hospital("Welcome Rumah Sakit Berkah","Jalan Ahamad yani no 21");
+
+Rs.welcome();

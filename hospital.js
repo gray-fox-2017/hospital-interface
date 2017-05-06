965a34300d284626e57f2e00e5243c1402ea6aa2
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-05-06 14:21:41 +0700

    hospital interface done

--- hospital.js
+++ hospital.js
@@ -1,3 +1,10 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  ouput: process.stdout,
+  prompt: "WELCOME!"
+})
+
 class Hospital {
   constructor(name, location, employees, patients) {
     this.name = name
